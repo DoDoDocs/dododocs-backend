1bae2f9532691a154179d3a44dc2520b20ddf2b9
msung99 <msung6924@naver.com>
2024-10-21 10:42:55 +0900

    feat: 백엔드 초기 개발환경 설정

---
+++ src/main/java/dododocs/dododocs/DododocsApplication.java
@@ -0,0 +1,13 @@
+package dododocs.dododocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DododocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DododocsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dododocs/dododocs/DododocsApplicationTests.java
@@ -0,0 +1,13 @@
+package dododocs.dododocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DododocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
