9858e6c639d963e32fd27cdd71eafb8fbb7cb798
msung99 <msung6924@naver.com>
2024-10-23 14:38:23 +0900

    feat: JWT 를 위한 의존성 추가

--- src/main/java/dododocs/dododocs/domain/JwtTokenProvider.java
+++ src/main/java/dododocs/dododocs/domain/JwtTokenProvider.java
@@ -3,8 +3,15 @@ package dododocs.dododocs.domain;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class JwtTokenProvider {
+    private final SecretKey secretKey;
+    private final long accessTokenValidityInSeconds;
 
+    public JwtTokenProvider(SecretKey secretKey, long accessTokenValidityInSeconds) {
+        this.secretKey = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+        this.accessTokenValidityInSeconds = accessTokenValidityInSeconds;
+    }
 }
