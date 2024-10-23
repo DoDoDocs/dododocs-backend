e1c5a0507984f3822ab4fde87e455bb12b6dd1f8
msung99 <msung6924@naver.com>
2024-10-23 14:10:02 +0900

    feat: JWT 토큰 제공자 생성

---
+++ src/main/java/dododocs/dododocs/domain/JwtTokenCreator.java
@@ -0,0 +1,16 @@
+package dododocs.dododocs.domain;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTokenCreator {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtTokenCreator(final JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    public String createToken(final long memberId) {
+        final String accessToken = jwtTokenProvider.
+    }
+}
---
+++ src/main/java/dododocs/dododocs/domain/JwtTokenProvider.java
@@ -0,0 +1,10 @@
+package dododocs.dododocs.domain;
+
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+
+@Component
+public class JwtTokenProvider {
+
+}
