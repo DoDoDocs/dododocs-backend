e97dadce4dbb4878f09400b014975d62eb646202
msung99 <msung6924@naver.com>
2024-10-31 11:18:09 +0900

    feat: 불필요한 인증 URI 제거

--- src/main/java/dododocs/dododocs/auth/application/AuthService.java
+++ src/main/java/dododocs/dododocs/auth/application/AuthService.java
@@ -9,6 +9,7 @@ import dododocs.dododocs.member.domain.Member;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+// https://park-algorithm.tistory.com/entry/Github-OAuth-%EB%A1%9C%EA%B7%B8%EC%9D%B8-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0#google_vignette
 @Transactional(readOnly = true)
 @Service
 public class AuthService {
--- src/main/java/dododocs/dododocs/domain/GithubOAuthUriProvider.java
+++ src/main/java/dododocs/dododocs/domain/GithubOAuthUriProvider.java
@@ -7,21 +7,15 @@ import org.springframework.stereotype.Component;
 public class GithubOAuthUriProvider {
     private final String authorizationUri;
     private final String clientId;
-    private final String redirectUri;
-    private final String responseType = "code";
 
     public GithubOAuthUriProvider(@Value("oauth.github.authorize_uri") final String authorizationUri,
-                              @Value("oauth.github.client_id") final String clientId,
-                              @Value("oauth.github.redirect_uri") final String redirectUri) {
+                              @Value("oauth.github.client_id") final String clientId) {
         this.authorizationUri = authorizationUri;
         this.clientId = clientId;
-        this.redirectUri = redirectUri;
     }
 
     public String generateUri() {
         return authorizationUri + "?"
-                + "client_id=" + clientId
-                + "&redirect_uri=" + redirectUri
-                + "&response_type=" + responseType;
+                + "client_id=" + clientId;
     }
 }
