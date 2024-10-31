f30e6f21731fc48761a470684f0f0bcf0e573821
msung99 <msung6924@naver.com>
2024-10-31 11:01:50 +0900

    feat: 소셜 로그인 제공처에 토큰 제공을 위한 OAuthAccessToken 생성

--- src/main/java/dododocs/dododocs/auth/infrastructure/OAuthAccessToken.java
+++ src/main/java/dododocs/dododocs/auth/infrastructure/OAuthAccessToken.java
@@ -1,8 +1,22 @@
 package dododocs.dododocs.auth.infrastructure;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
 public class OAuthAccessToken {
+    @JsonProperty("access_token")
     private String accessToken;
+
+    @JsonProperty("expires_in")
+    private int expiresIn;
+
+    @JsonProperty("token_type")
+    private String tokenType;
+
+    @JsonProperty("scope")
+    private String scope;
+
+    @JsonProperty("refesh_token")
+    private String refreshToken;
 }
