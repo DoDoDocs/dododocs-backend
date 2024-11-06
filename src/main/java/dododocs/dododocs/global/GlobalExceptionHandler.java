d9fa7b2b8b1364d76e8e953e0c90d17f48052cdc
msung99 <msung6924@naver.com>
2024-11-06 14:25:46 +0900

    feat: HTTP 예외처리

--- src/main/java/dododocs/dododocs/global/GlobalExceptionHandler.java
+++ src/main/java/dododocs/dododocs/global/GlobalExceptionHandler.java
@@ -1,9 +1,13 @@
 package dododocs.dododocs.global;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @Slf4j
 @RestControllerAdvice
@@ -12,6 +16,24 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ExceptionResponse> handleRunTimeException(RuntimeException e) {
         log.error(e.getMessage(), e);
         ExceptionResponse exceptionResponse = new ExceptionResponse(e.getMessage());
+        return ResponseEntity.badRequest().body(exceptionResponse);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ExceptionResponse> handleInvalidRequestBody() {
+        ExceptionResponse exceptionResponse = new ExceptionResponse("잘못된 Request Body 형식 입니다.");
+        return ResponseEntity.badRequest().body(exceptionResponse);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ExceptionResponse> handleNotSupportedMethod() {
+        ExceptionResponse errorResponse = new ExceptionResponse("잘못된 HTTP 메소드 요청입니다.");
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(errorResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ExceptionResponse> handleTypeMismatch() {
+        ExceptionResponse exceptionResponse = new ExceptionResponse("잘못된 타입을 가진 데이터가 포함되어 있습니다.");
         return ResponseEntity.badRequest().body(exceptionResponse);
     }
 
