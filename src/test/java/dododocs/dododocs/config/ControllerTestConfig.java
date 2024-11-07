f7139ebafa638bb26fc588ffb26711cef08fc86a
msung99 <msung6924@naver.com>
2024-11-07 09:46:25 +0900

    feat: 컨트롤러 테스트 설정을 위한 Config 생성

---
+++ src/test/java/dododocs/dododocs/config/ControllerTestConfig.java
@@ -0,0 +1,38 @@
+package dododocs.dododocs.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dododocs.dododocs.auth.application.AuthService;
+import dododocs.dododocs.auth.infrastructure.GithubOAuthClient;
+import dododocs.dododocs.auth.presentation.AuthController;
+import dododocs.dododocs.auth.presentation.authentication.AuthenticationBearerExtractor;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+@AutoConfigureRestDocs
+@WebMvcTest({
+        AuthController.class,
+})
+@Import(TestConfig.class)
+@ActiveProfiles("test")
+public abstract class ControllerTestConfig {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @MockBean
+    protected AuthService authService;
+
+    @Mock
+    protected GithubOAuthClient githubOAuthClient;
+
+    @MockBean
+    protected AuthenticationBearerExtractor authenticationBearerExtractor;
+}
---
+++ src/test/java/dododocs/dododocs/config/TestConfig.java
@@ -0,0 +1,10 @@
+package dododocs.dododocs.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@TestConfiguration
+public class TestConfig {
+
+}
+
