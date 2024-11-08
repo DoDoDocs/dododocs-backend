9efab97e146d2a41ff7401f8c94ed98eb425bbb3
msung99 <msung6924@naver.com>
2024-11-08 16:47:28 +0900

    feat: runner 를 통한 환경변수 로깅

---
+++ src/main/java/dododocs/dododocs/runner/DocsApplicationRunner.java
@@ -0,0 +1,20 @@
+package dododocs.dododocs.runner;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.beans.factory.annotation.Value;
+
+@Slf4j
+@RequiredArgsConstructor
+public class DocsApplicationRunner implements ApplicationRunner {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.info("DATABASE URL: {}", dbUrl);
+    }
+}
