5c26411fc1f88f543ebafee9c496bea59f137517
msung99 <msung6924@naver.com>
2024-11-17 20:43:43 +0900

    feat: MemberOrganization 도메인 생성

---
+++ src/main/java/dododocs/dododocs/analyze/MemberOrganization.java
@@ -0,0 +1,20 @@
+package dododocs.dododocs.analyze;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberOrganization {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    @Column(name = "name")
+    private String name;
+}
