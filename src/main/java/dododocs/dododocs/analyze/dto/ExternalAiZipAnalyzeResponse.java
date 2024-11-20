18548bf01b4ba2da7e1b1e88bf6a109093c7fd13
msung99 <msung6924@naver.com>
2024-11-20 16:22:51 +0900

    feat: DTO 정의

--- src/main/java/dododocs/dododocs/analyze/dto/ExternalAiZipAnalyzeResponse.java
+++ src/main/java/dododocs/dododocs/analyze/dto/ExternalAiZipAnalyzeResponse.java
@@ -1,4 +1,22 @@
 package dododocs.dododocs.analyze.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+@Getter
 public class ExternalAiZipAnalyzeResponse {
+
+    @JsonProperty("readm3_s3_key")
+    private String readM3S3Key;
+
+    @JsonProperty("docs_s3_key")
+    private String docsS3Key;
+
+    private ExternalAiZipAnalyzeResponse() {
+    }
+
+    public ExternalAiZipAnalyzeResponse(final String readM3S3Key, final String docsS3Key) {
+        this.readM3S3Key = readM3S3Key;
+        this.docsS3Key = docsS3Key;
+    }
 }
