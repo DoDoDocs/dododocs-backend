b62f020a69bc04632bc4508dba84a8a0371e0adb
msung99 <msung6924@naver.com>
2024-11-21 10:42:28 +0900

    docs: 명세할 리스트 작성

---
+++ src/test/java/dododocs/dododocs/analyze/DownloadControllerTest.java
@@ -0,0 +1,19 @@
+package dododocs.dododocs.analyze;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DownloadControllerTest {
+
+    @DisplayName("AI 분석 결과를 다운로드 받고 상태코드 200을 리턴한다.")
+    @Test
+    void a() throws Exception {
+
+    }
+
+    @DisplayName("아직 AI 분석 결과가 완료되지 않았다면 상태코드 400을 리턴한다.")
+    @Test
+    void b() throws Exception {
+
+    }
+}
