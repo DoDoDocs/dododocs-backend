b45a016b93591628d9afeb559438acedb14ac5d6
msung99 <msung6924@naver.com>
2024-11-25 15:27:19 +0900

    feat: prefix

--- src/main/java/dododocs/dododocs/analyze/infrastructure/ExternalAiZipAnalyzeClient.java
+++ src/main/java/dododocs/dododocs/analyze/infrastructure/ExternalAiZipAnalyzeClient.java
@@ -8,17 +8,21 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
 public class ExternalAiZipAnalyzeClient {
-    private static final String AI_ZIP_DOWNLOAD_AND_ANALYZE_REQUEST_URL = "http://localhost:8000/travel/custom/model?page={page}";
+    private final String aiBasicUrl;
+    private final String AI_ZIP_DOWNLOAD_AND_ANALYZE_REQUEST_URL_PREFIX = "travel/custom/model?page={page}";
     private final RestTemplate restTemplate;
 
-    public ExternalAiZipAnalyzeClient(final RestTemplate restTemplate) {
+    public ExternalAiZipAnalyzeClient(final RestTemplate restTemplate,
+                                      @Value("${ai.basic_url}") final String aiBasicUrl) {
         this.restTemplate = restTemplate;
+        this.aiBasicUrl = aiBasicUrl;
     }
 
     public ExternalAiZipAnalyzeResponse requestAiZipDownloadAndAnalyze(final ExternalAiZipAnalyzeRequest request) {
@@ -29,7 +33,7 @@ public class ExternalAiZipAnalyzeClient {
         final Map<String, String> uriVariables = new HashMap<>();
 
         final ResponseEntity<ExternalAiZipAnalyzeResponse> responseEntity = restTemplate.exchange(
-                AI_ZIP_DOWNLOAD_AND_ANALYZE_REQUEST_URL,
+                aiBasicUrl + AI_ZIP_DOWNLOAD_AND_ANALYZE_REQUEST_URL_PREFIX,
                 HttpMethod.POST,
                 new HttpEntity<>(request),
                 ExternalAiZipAnalyzeResponse.class,
