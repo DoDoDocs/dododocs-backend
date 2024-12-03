53dff945de60834191b096db0c5a1d2ec32e7649
msung99 <msung6924@naver.com>
2024-12-03 20:28:51 +0900

    fix: 업로드 DTO 변경

--- src/main/java/dododocs/dododocs/analyze/dto/UploadGitRepoContentToS3Request.java
+++ src/main/java/dododocs/dododocs/analyze/dto/UploadGitRepoContentToS3Request.java
@@ -10,4 +10,31 @@ import lombok.NoArgsConstructor;
 public class UploadGitRepoContentToS3Request {
     private String repositoryName;
     private String branchName;
+    private boolean previewBlock;
+    private boolean overviewBlock;
+    private boolean analysisBlock;
+    private boolean structureBlock;
+    private boolean startBlock;
+    private boolean motivationBlock;
+    private boolean demoBlock;
+    private boolean deploymentBlock;
+    private boolean contributorsBlock;
+    private boolean faqBlock;
+    private boolean performanceBlock;
 }
+
+/*
+README_BLOCKS = {
+    "PREVIEW_BLOCK": PREVIEW_BLOCK,
+    "OVERVIEW_BLOCK": OVERVIEW_BLOCK,
+    "ANALYSIS_BLOCK": ANALYSIS_BLOCK,
+    "STRUCTURE_BLOCK": STRUCTURE_BLOCK,
+    "START_BLOCK": START_BLOCK,
+    "MOTIVATION_BLOCK": MOTIVATION_BLOCK,
+    "DEMO_BLOCK": DEMO_BLOCK,
+    "DEPLOYMENT_BLOCK": DEPLOYMENT_BLOCK,
+    "CONTRIBUTORS_BLOCK": CONTRIBUTORS_BLOCK,
+    "FAQ_BLOCK": FAQ_BLOCK,
+    "PERFORMANCE_BLOCK": PERFORMANCE_BLOCK,
+}
+ */
