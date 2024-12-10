f2918caeca560ff5d3f48adacd6f37979ac83bd1
msung99 <msung6924@naver.com>
2024-12-10 18:12:53 +0900

    feat: 챗봇 질문 비즈니스 로직

--- src/main/java/dododocs/dododocs/chatbot/application/ChatbotService.java
+++ src/main/java/dododocs/dododocs/chatbot/application/ChatbotService.java
@@ -2,15 +2,16 @@ package dododocs.dododocs.chatbot.application;
 
 import dododocs.dododocs.chatbot.dto.QuestToChatbotRequest;
 import dododocs.dododocs.chatbot.dto.QuestToChatbotResponse;
+import dododocs.dododocs.chatbot.infrastructure.ExternalChatbotClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class ChatbotService {
-    private
+    private ExternalChatbotClient externalChatbotClient;
 
     public QuestToChatbotResponse questionToChatbotAndSaveLogs(final QuestToChatbotRequest questToChatbotRequest) {
-        return
+        return externalChatbotClient.questToChatbot(questToChatbotRequest);
     }
 }
