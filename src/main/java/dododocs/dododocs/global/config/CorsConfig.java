ae851d58c4b25a68fed46a9b776b0b12c2b28423
msung99 <msung6924@naver.com>
2024-12-13 16:28:09 +0900

    chore: cors 추가

--- src/main/java/dododocs/dododocs/global/config/CorsConfig.java
+++ src/main/java/dododocs/dododocs/global/config/CorsConfig.java
@@ -11,7 +11,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:8000", "https://kcfaa61d53ebba.user-app.krampoline.com", "https://kcfaa61d53ebba.user-app.krampoline.com:3000")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8000",
+                        "https://dododocs.com", "https://dododocs.com:3000", "https://dododocs.com:443",
+                        "https://kcfaa61d53ebba.user-app.krampoline.com", "https://kcfaa61d53ebba.user-app.krampoline.com:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowCredentials(true)
                 .exposedHeaders(HttpHeaders.LOCATION);
