7015b595d5bb536bc3131d540f616b64551f0c6b
msung99 <msung6924@naver.com>
2024-12-16 16:56:42 +0900

    docs: 레포 등록날짜 서식 수정

--- src/main/java/dododocs/dododocs/member/dto/FindRegisterRepoResponse.java
+++ src/main/java/dododocs/dododocs/member/dto/FindRegisterRepoResponse.java
@@ -3,20 +3,26 @@ package dododocs.dododocs.member.dto;
 import dododocs.dododocs.analyze.domain.RepoAnalyze;
 import lombok.Getter;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
 public class FindRegisterRepoResponse {
     private String repositoryName;
     private String branchName;
-    private LocalDateTime createdAt;
+    private LocalDate createdAt;
 
     private FindRegisterRepoResponse() {
     }
 
     public FindRegisterRepoResponse(final RepoAnalyze repoAnalyze) {
         this.repositoryName = repoAnalyze.getRepositoryName();
-        this.createdAt = repoAnalyze.getCreatedAt();
         this.branchName = repoAnalyze.getBranchName();
+
+        if(repoAnalyze.getCreatedAt() != null) {
+            this.createdAt = LocalDate.from(repoAnalyze.getCreatedAt());
+        } else {
+            this.createdAt = LocalDate.now();
+        }
     }
 }
