50136c41e5a7e1c1ae92a5f28c798977567bb452
msung99 <msung6924@naver.com>
2024-12-16 16:40:41 +0900

    test: 존재하지 않는 레포지토리 또는 브랜치를 입력시 상태코드 404를 리턴

--- src/test/java/dododocs/dododocs/analyze/AnalyzeControllerTest.java
+++ src/test/java/dododocs/dododocs/analyze/AnalyzeControllerTest.java
@@ -3,6 +3,7 @@ package dododocs.dododocs.analyze;
 import dododocs.dododocs.analyze.dto.FindGitRepoContentRequest;
 import dododocs.dododocs.analyze.dto.UploadGitRepoContentToS3Request;
 import dododocs.dododocs.analyze.exception.MaxSizeRepoRegiserException;
+import dododocs.dododocs.analyze.exception.NoExistGitRepoException;
 import dododocs.dododocs.config.ControllerTestConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -64,10 +65,10 @@ public class AnalyzeControllerTest extends ControllerTestConfig {
 
         // when, then
         mockMvc.perform(post("/api/upload/s3")
-                .header("Authorization", "Bearer aaaaaa.bbbbbb.cccccc")
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(new UploadGitRepoContentToS3Request("Gatsby-Starter-Haon", "main", true, true))))
+                        .header("Authorization", "Bearer aaaaaa.bbbbbb.cccccc")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(new UploadGitRepoContentToS3Request("Gatsby-Starter-Haon", "main", true, true))))
                 .andDo(print())
                 .andDo(document("analyze/upload/fail/max/size",
                         preprocessRequest(prettyPrint()),
@@ -80,8 +81,38 @@ public class AnalyzeControllerTest extends ControllerTestConfig {
                                 fieldWithPath("branchName").type(JsonFieldType.STRING).description("브랜치 명 (ex. main)"),
                                 fieldWithPath("korean").type(JsonFieldType.BOOLEAN).description("한국어 번역 여부"),
                                 fieldWithPath("includeTest").type(JsonFieldType.BOOLEAN).description("테스트 포함 여부")
-                )))
+                        )))
                 .andExpect(status().isBadRequest());
+    }
+
+    @DisplayName("존재하지 않는 레포지토리 또는 브랜치를 입력시 상태코드 404를 리턴한다.")
+    @Test
+    void registerNotFoundGitRepoException() throws Exception {
+        // given
+        given(jwtTokenCreator.extractMemberId(anyString())).willReturn(1L);
+        doThrow(new NoExistGitRepoException("존재하지 않는 레포지토리 또는 브랜치입니다."))
+                .when(analyzeService).uploadGithubRepoToS3(any(), anyLong());
+
+        // when, then
+        mockMvc.perform(post("/api/upload/s3")
+                        .header("Authorization", "Bearer aaaaaa.bbbbbb.cccccc")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(new UploadGitRepoContentToS3Request("Invalid-Git-Repo-Name", "main222", true, true))))
+                .andDo(print())
+                .andDo(document("analyze/upload/fail/max/size",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestHeaders(
+                                headerWithName("Authorization").description("엑세스 토큰")
+                        ),
+                        requestFields(
+                                fieldWithPath("repositoryName").type(JsonFieldType.STRING).description("분석 할 레포지토리 이름 (ex. Gatsby-Starter-Haon)"),
+                                fieldWithPath("branchName").type(JsonFieldType.STRING).description("브랜치 명 (ex. main)"),
+                                fieldWithPath("korean").type(JsonFieldType.BOOLEAN).description("한국어 번역 여부"),
+                                fieldWithPath("includeTest").type(JsonFieldType.BOOLEAN).description("테스트 포함 여부")
+                        )))
+                .andExpect(status().isNotFound());
     }
 
     @DisplayName("레포지토리의 모든 코드를 읽어오고 상태코드 200을 리턴한다.")
